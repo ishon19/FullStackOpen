bcbc8c3180967f714c424b5e346e35cd507095ad
ishon19 <pathak.shreyans@gmail.com>
2020-11-17 07:44:01 +0530

    modified the baseURL

---
+++ part3/PhonebookServer/build/precache-manifest.29d099eb052f2a7be186e6dcf821b684.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c96b9d2b3e6d4cd37d1336f3ce1eba3b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4b8b833d9c4c7af7036f",
+    "url": "/static/css/main.f97f9d52.chunk.css"
+  },
+  {
+    "revision": "638ef986ed80be7d2ba3",
+    "url": "/static/js/2.26de4d96.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.26de4d96.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4b8b833d9c4c7af7036f",
+    "url": "/static/js/main.91a50208.chunk.js"
+  },
+  {
+    "revision": "f4d2ac87a76b6331fd91",
+    "url": "/static/js/runtime-main.4234e0d5.js"
+  }
+]);
