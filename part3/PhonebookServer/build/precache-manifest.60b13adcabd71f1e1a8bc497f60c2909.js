019028f19161cf9fafb7977c56580401fb7445a1
ishon19 <pathak.shreyans@gmail.com>
2020-11-20 13:28:21 +0530

    build updated

---
+++ part3/PhonebookServer/build/precache-manifest.60b13adcabd71f1e1a8bc497f60c2909.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "90d22e5e889826a5db24358f2b1f5280",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7c8ef173959c4e71044e",
+    "url": "/static/css/main.f97f9d52.chunk.css"
+  },
+  {
+    "revision": "638ef986ed80be7d2ba3",
+    "url": "/static/js/2.26de4d96.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.26de4d96.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7c8ef173959c4e71044e",
+    "url": "/static/js/main.3470ae28.chunk.js"
+  },
+  {
+    "revision": "f4d2ac87a76b6331fd91",
+    "url": "/static/js/runtime-main.4234e0d5.js"
+  }
+]);
